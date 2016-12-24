5c6f3eaefe79308c8fc59df8e0408625a6224a46
aldebaran1 <smrak@bu.edu>
2016-12-23 19:28:12 -0500

    s

--- mahali_paper.py
+++ mahali_paper.py
@@ -18,6 +18,7 @@ import plotGps
 import plotting
 import plotSatellite
 import magnetometer
+import matplotlib.pyplot as plt
 
 #Type paths to raw data
 
@@ -25,20 +26,34 @@ receiver = {2:'mah22800.h5', 3:'mah32800.h5', 4:'mah42800.h5', 5:'mah52800.h5',
             6:'mah62800.h5', 7:'mah72800.h5', 8:'mah82800.h5', 9:'mah92800.h5',
             13:'ma132800.h5'}
             
-yaml = {2:'mah22800.yaml', 3:'mah32800.yaml', 4:'mah42800.yaml', 5:'mah52800.yaml',
+yml = {2:'mah22800.yaml', 3:'mah32800.yaml', 4:'mah42800.yaml', 5:'mah52800.yaml',
             6:'mah62800.yaml', 7:'mah72800.yaml', 8:'mah82800.yaml', 9:'mah92800.yaml',
             13:'ma132800.yaml'}
 
 fn = '/home/smrak/Documents/TheMahali/rinex/'
 asi_folder3 = '/home/smrak/Documents/TheMahali/Allsky_multi/'
-fname = '/home/smrak/Documents/TheMahali/rinex/mah82800.h5'
+
 navfname = '/home/smrak/Documents/TheMahali/gnss/gps/brdc2800.15n'
 timelim = ['10/07/2015', '06:08:00', '10/07/2015', '06:50:00']
+skip = 500
 sv = 23
-rx = 8
 ipp_alt = 130
-stream = yaml.load(open(fn+'mah82800.yaml', 'r'))
-rx_xyz = stream.get('APPROX POSITION XYZ')
-data = read_hdf(fname)
+green_alt = 120
+red_alt = 250
+blue_alt = 90
 obstimes = np.array((data.major_axis))
 obstimes = pandas.to_datetime(obstimes) 
+
+rx = 8
+fname = fn + str(receiver.get(rx))
+stream = yaml.load(open(fn + str(yml.get(rx)), 'r'))
+rx_xyz = stream.get('APPROX POSITION XYZ')
+data = read_hdf(fname)
+
+L1 = np.array(data['L1', sv, skip:, 'data'])*2*np.pi
+idx = np.where(np.isfinite(L1))[0]
+L1 = L1[idx]
+x = range(L1.shape[0])
+L1_d = pyGps.phaseDetrend(L1, 9)
+Y = pyGps.scintHpf(L1_d, 0.05, 6)
+plt.plot(obstimes[idx], Y)
